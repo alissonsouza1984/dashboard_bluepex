0d90e3faa66482597b193dc423d837cb6435841c
alissonsouza1984 <alissonseginf@gmail.com>
2023-08-22 12:11:32 -0300

    add menssagens login

--- includes/scripts/login_process.php
+++ includes/scripts/login_process.php
@@ -1,8 +1,7 @@
 <?php
 session_start();
-require_once '../../config/db.php'; // Caminho para o arquivo de configuração do banco de dados
+require_once '../../config/db.php';
 
-// Exibir erros no navegador (apenas para ambiente de desenvolvimento)
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
 
@@ -17,10 +16,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $user = $stmt->fetch(PDO::FETCH_ASSOC);
 
     if ($user && password_verify($password, $user['password'])) {
-        // Iniciar sessão e armazenar ID do usuário na variável de sessão
         $_SESSION['user_id'] = $user['id'];
-
-        // Redirecionar para a página da dashboard
         header("Location: ../../pages/dashboard.php");
         exit();
     } else {
